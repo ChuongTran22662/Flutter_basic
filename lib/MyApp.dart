0672274798a327ac186d6807b8b493b85700a018
Tran Dao Quang Chuong <chuongcongtu9x@gmail.com>
2020-06-14 21:13:04 +0700

    StatefulWidget and change state with TextField

---
+++ lib/MyApp.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+//Mỗi Statefuk widget được tạo thành từ 2 class sau :
+//- Một public class kế thừa tiwf StatefulWidget
+//-Một private class để vẽ giao diện (qua hàm build)
+class MyApp extends StatefulWidget {
+  String name;
+  int age;
+
+  MyApp({this.name, this.age});
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+//  String name;
+//  int age;
+//
+//  MyApp({this.name, this.age});
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return MaterialApp(
+//      title: 'This is my first app',
+//      home: Scaffold(
+//          body: Center(
+//              child: Text(
+//                'name : ${this.name}, age : ${this.age}',
+//                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+//                textDirection: TextDirection.ltr,
+//              ))),
+//    );
+//  }
+}
+
+class _MyAppState extends State<MyApp> {
+  String _email = '';
+  final emailEditingController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'This ia a StatefulWidget',
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: TextField(
+                  controller: emailEditingController,
+                  onChanged: (text) {
+                    this.setState(() {
+                      _email = text;
+                    });
+                  },
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius:
+                            const BorderRadius.all(const Radius.circular(15))),
+                    labelText: 'Enter your email',
+                  ),
+                ),
+              ),
+              Text(
+                'Result',
+                style: TextStyle(fontSize: 30, color: Colors.blue),
+              ),
+              Text(
+                _email,
+                style: TextStyle(fontSize: 30, color: Colors.green),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
